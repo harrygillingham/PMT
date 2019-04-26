37b439b44a08c10d99f5fcfd49cacf59afe9dc1b
Alex <alexcchalke@gmail.com>
2019-04-26 13:54:59 +0100

    Completed activities

--- app/src/main/java/com/example/gillingham/pmt/NewGatheringActivity.java
+++ app/src/main/java/com/example/gillingham/pmt/NewGatheringActivity.java
@@ -1,14 +1,15 @@
 package com.example.gillingham.pmt;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 public class NewGatheringActivity extends AppCompatActivity {
 
@@ -19,14 +20,7 @@ public class NewGatheringActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
     }
 
     @Override
@@ -69,6 +63,27 @@ public class NewGatheringActivity extends AppCompatActivity {
                 return super.onOptionsItemSelected(item);
 
         }
+    }
+
+    public void popUpShow(View view) {
+        String[] colors = {"1 day", "7 Days", "2 Weeks", "1 Month"};
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("How long do you want to sponsor for.");
+        builder.setItems(colors, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        builder.show();
+        Toast.makeText(this, "Event Created!",
+                Toast.LENGTH_LONG).show();
+    }
+
+    public void toastShow(View view) {
+        Toast.makeText(this, "Event Created!",
+                Toast.LENGTH_LONG).show();
     }
 
 }
